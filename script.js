102ad4b07644869b156637d7a11b4457d5e9a09f
Daniel <danstraws>
2023-06-27 02:07:18 -0500

    major rework to css files. all styles come from one file. increased robustness; resizing the window will not impact experience

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// Update the height of the content div based on the window size
+function adjustContentHeight() {
+    const content = document.getElementById('content');
+    const windowHeight = window.innerHeight;
+    content.style.height = `${windowHeight}px`;
+  }
+  
+  // Call the adjustContentHeight function initially and on window resize
+  window.addEventListener('resize', adjustContentHeight);
+  adjustContentHeight();
+  
